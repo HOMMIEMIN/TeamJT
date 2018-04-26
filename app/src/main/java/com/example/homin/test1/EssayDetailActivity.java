98f1981841dcbfe3cf88926f6429f33c4186c763
Joominseok <alstjr@naver.com>
2018-04-26 10:02:02 +0900

    .

---
+++ app/src/main/java/com/example/homin/test1/EssayDetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.homin.test1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EssayDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_essay_detail);
+    }
+}
