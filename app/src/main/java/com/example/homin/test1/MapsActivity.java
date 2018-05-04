e03e8bcdb123686f35de1e474af97c2b2e6e0a69
minnie-J <tigerleo7@naver.com>
2018-05-04 13:42:55 +0900

    끝!

--- app/src/main/java/com/example/homin/test1/MapsActivity.java
+++ app/src/main/java/com/example/homin/test1/MapsActivity.java
@@ -1129,7 +1129,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         realFriendList.add(DaoImple.getInstance().getLoginEmail());
 
                         if(targetIdMarker!= null) {
-                            clusterManager.addItem(targetMarker);
+                            clusterManager.addItem(targetIdMarker);
                             clusterManager.cluster();
                         }
                         if (!memoAddCheck) {
